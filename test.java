35dc066a5a2edf37b41770122f879d4945e6bb27
Muhammad Khuzaima Umair <mkhuzaimaumair@gmail.com>
2023-01-14 17:58:17 +0500

    test file added

---
+++ test.java
@@ -0,0 +1,8 @@
+// id: 2246
+// Name: Longest Path With Different Adjacent Characters
+// link: https://leetcode.com/problems/longest-path-with-different-adjacent-characters/
+// Difficulty: Hard
+
+public class test {
+    
+}
