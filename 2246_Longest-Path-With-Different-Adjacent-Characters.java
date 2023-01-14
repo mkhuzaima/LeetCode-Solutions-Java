c63e16609f95da587492898c1f22ba0be3e84663
Muhammad Khuzaima Umair <mkhuzaima@users.noreply.github.com>
2023-01-14 13:12:22 +0000

    Formatted 2246_Longest-Path-With-Different-Adjacent-Characters.java

--- 2246_Longest-Path-With-Different-Adjacent-Characters.java
+++ 2246_Longest-Path-With-Different-Adjacent-Characters.java
@@ -1,7 +1,13 @@
-// id: 2246
-// Name: Longest Path With Different Adjacent Characters
-// link: https://leetcode.com/problems/longest-path-with-different-adjacent-characters/
-// Difficulty: Hard
+/*
+ * 2246. Longest Path With Different Adjacent Characters
+ * Problem Link: https://leetcode.com/problems/longest-path-with-different-adjacent-characters/
+ * Difficulty:   Hard
+ * 
+ * Solution Created by: Muhammad Khuzaima Umair
+ * LeetCode : https://leetcode.com/mkhuzaima/
+ * Github   : https://github.com/mkhuzaima
+ * LinkedIn : https://www.linkedin.com/in/mkhuzaima/
+ */
 
 class Solution {
 
