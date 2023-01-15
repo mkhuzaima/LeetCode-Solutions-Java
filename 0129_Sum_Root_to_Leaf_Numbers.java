5057b5ef0dfe75c977b11a412b2b33eda31f1488
Muhammad Khuzaima Umair <mkhuzaimaumair@gmail.com>
2023-01-15 10:27:59 +0500

    Problem 129 Added

---
+++ 0129_Sum_Root_to_Leaf_Numbers.java
@@ -0,0 +1,41 @@
+// id: 129
+// Name: Sum Root to Leaf Numbers
+// link: https://leetcode.com/problems/sum-root-to-leaf-numbers
+// Difficulty: Medium
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    // method to calculate the sum of all the numbers formed by the path from the root to a leaf node
+    public int sumNumbers(TreeNode root) {
+        // call the helper method to traverse the tree and calculate the sum
+        return traversal(root, 0);
+    }
+
+    private int traversal(TreeNode root, int parentValue) {
+        // base case: if current node is null, return 0
+        if (root == null) return 0;
+        
+        // if current node is a leaf node, return the value of the path from the root to this leaf node
+        if (root.left == null && root.right == null) {
+            return 10*parentValue + root.val;
+        }
+        
+        // recursively call the traversal method on left and right subtrees and add the returned values
+        return traversal(root.left, 10*parentValue + root.val) +
+            traversal(root.right, 10*parentValue + root.val);
+    }
+}
