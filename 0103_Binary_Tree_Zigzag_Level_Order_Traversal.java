41aa07e4b03bdfddd04779282695389f1612e65d
Muhammad Khuzaima Umair <75787251+mkhuzaima@users.noreply.github.com>
2023-02-19 17:28:54 +0500

    Problem 103 added

---
+++ 0103_Binary_Tree_Zigzag_Level_Order_Traversal.java
@@ -0,0 +1,42 @@
+// id: 103
+// Name: Binary Tree Zigzag Level Order Traversal
+// link: https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+// Difficulty: Medium
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<>();
+        traverse(root, list, 0, false);
+
+        return list;
+    }
+    private void traverse(TreeNode root, List<List<Integer>> list, int level, boolean toAddAtStart) {
+        if (root == null) return;
+
+        if (list.size() <= level) {
+            list.add(new ArrayList<>());
+        }
+        if (toAddAtStart) {
+            list.get(level).add(0, root.val);
+        }
+        else {
+            list.get(level).add(root.val);
+        }
+        traverse(root.left, list, level+1, !toAddAtStart);
+        traverse(root.right, list, level+1, !toAddAtStart);
+    }
+}
