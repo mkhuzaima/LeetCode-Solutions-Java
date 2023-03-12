b58e60961d501e8747e8a187382f7086114b1031
Muhammad Khuzaima Umair <75787251+mkhuzaima@users.noreply.github.com>
2023-03-13 00:39:07 +0500

    Problem 21 Added

---
+++ 0021_Merge_Two_Sorted_Lists.java
@@ -0,0 +1,35 @@
+// id: 21
+// Name: Merge Two Sorted Lists
+// link: https://leetcode.com/problems/merge-two-sorted-lists/
+// Difficulty: Easy
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        // make dummy node
+        ListNode head = new ListNode(-1);
+        ListNode tail = head;
+        
+        
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                tail.next = list1;
+                list1 = list1.next;
+            }
+            else {
+                tail.next = list2;
+                list2 = list2.next;
+            }
+            tail = tail.next;
+        }
+
+        // only 1 of the following if will run
+        if (list1 != null) {
+            tail.next = list1;
+        }
+        if (list2 != null) {
+            tail.next = list2;
+        }
+
+        return head.next;
+    }
+}
