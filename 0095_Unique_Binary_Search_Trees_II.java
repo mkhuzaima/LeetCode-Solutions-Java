8d372a9c565e43d1bb0172a5404dc16c56c5c09d
Muhammad Khuzaima Umair <mkhuzaimaumair@gmail.com>
2023-03-18 18:31:28 +0500

    Added problem 95

---
+++ 0095_Unique_Binary_Search_Trees_II.java
@@ -0,0 +1,40 @@
+// id: 95
+// Name: Unique Binary Search Trees II
+// link: https://leetcode.com/problems/unique-binary-search-trees-ii/
+// Difficulty: Medium
+
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        // Calls the recursive helper function to generate subtrees from 1 to n
+        return generateSubtrees(1, n);
+    }
+
+    private List<TreeNode> generateSubtrees(int low, int high) {
+        // Initialize an empty result list
+        List<TreeNode> result = new ArrayList<>();
+
+        // If low is greater than high, the subtree is empty, so add null to the result list
+        if (low > high) {
+            result.add(null);
+            return result;
+        }
+
+        // Generate all possible subtrees by iterating from low to high
+        for (int i = low; i <= high; i++) {
+            // Recursively generate all possible left subtrees
+            List<TreeNode> leftList = generateSubtrees(low, i-1);
+
+            // Recursively generate all possible right subtrees
+            List<TreeNode> rightList = generateSubtrees(i+1, high);
+
+            // Merge the left, current, and right subtrees to create new trees
+            for (TreeNode left: leftList) {
+                for (TreeNode right: rightList) {
+                    // Create a new tree with current node i and left and right subtrees
+                    result.add(new TreeNode(i, left, right));
+                }
+            }
+        }
+        return result;
+    }
+}
