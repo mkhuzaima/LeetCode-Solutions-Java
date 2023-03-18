1f88ff6ef8943e85ba03fbc5400e9330366c8f81
Muhammad Khuzaima Umair <mkhuzaimaumair@gmail.com>
2023-03-18 15:50:51 +0500

    Added problem 1472

---
+++ 1472_Design_Browser_History.java
@@ -0,0 +1,60 @@
+// id: 1472
+// Name: Design Browser History
+// link: https://leetcode.com/problems/design-browser-history/
+// Difficulty: Medium
+
+class BrowserHistory {
+    
+    // Stack to store the URLs visited after the current page
+    Stack<String> forwardHistory;
+    
+    // Stack to store the URLs visited before the current page
+    Stack<String> backwardHistory;
+    
+    // The current page URL
+    String currentPage;
+
+    // Constructor to initialize the browser with a homepage
+    public BrowserHistory(String homepage) {
+        forwardHistory = new Stack<>();
+        backwardHistory = new Stack<>();
+        currentPage = homepage;
+    }
+    
+    // Method to visit a new page
+    public void visit(String url) {
+        
+        // Clear the forward history since we are starting a new chain of visited URLs
+        forwardHistory.clear();
+
+        // Add the current page to the backward history and set the current page to the new URL
+        backwardHistory.push(currentPage);
+        currentPage = url;
+    }
+    
+    // Method to move backward by a certain number of steps
+    public String back(int steps) {
+        // Move back by popping URLs from the backward history and pushing them onto the forward history
+        while (steps > 0 && !backwardHistory.isEmpty()) {
+            forwardHistory.push(currentPage);
+            currentPage = backwardHistory.pop();
+            steps--;
+        }
+        
+        // Return the current page after moving back
+        return currentPage;
+    }
+    
+    // Method to move forward by a certain number of steps
+    public String forward(int steps) {
+        // Move forward by popping URLs from the forward history and pushing them onto the backward history
+        while (steps > 0 && !forwardHistory.isEmpty()) {
+            backwardHistory.push(currentPage);
+            currentPage = forwardHistory.pop();
+            steps--;
+        }
+        
+        // Return the current page after moving forward
+        return currentPage;
+    }
+}
