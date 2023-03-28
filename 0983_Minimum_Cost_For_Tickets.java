66764ac3ac55fcb60833c2ca32d350dac3a33c81
Muhammad Khuzaima Umair <75787251+mkhuzaima@users.noreply.github.com>
2023-03-28 23:28:32 +0500

    Create 0983_Minimum_Cost_For_Tickets.java

---
+++ 0983_Minimum_Cost_For_Tickets.java
@@ -0,0 +1,26 @@
+// id: 983
+// Name: Minimum Cost For Tickets
+// link: https://leetcode.com/problems/minimum-cost-for-tickets/
+// Difficulty: Medium
+
+class Solution {
+    public int mincostTickets(int[] days, int[] costs) {
+        Queue<int[]> last7 = new LinkedList<>();    // stores last 7 days
+        Queue<int[]> last30 = new LinkedList<>();   // stores last 30 days
+        int cost = 0;
+
+        for (int d : days) {
+            while (!last7.isEmpty() && d -7 >= last7.peek()[0]) {
+                last7.poll();
+            }
+            while (!last30.isEmpty() && d-30 >= last30.peek()[0]) {
+                last30.poll();
+            }
+            last7.offer(new int[]{d, cost + costs[1]});
+            last30.offer(new int[]{d, cost + costs[2]});
+            cost = Math.min(cost + costs[0], Math.min(last7.peek()[1], last30.peek()[1]));
+        }
+
+        return cost;
+    }
+}
