0cc72ce1b83373f0789d5b58dbb5713085a7dbb8
Muhammad Khuzaima Umair <mkhuzaimaumair@gmail.com>
2023-06-11 18:05:18 +0500

    Create 2326_Spiral_Matrix_IV.java

---
+++ 2326_Spiral_Matrix_IV.java
@@ -0,0 +1,58 @@
+// id: 2326
+// Name: Spiral Matrix IV
+// link: https://leetcode.com/problems/spiral-matrix-iv/
+// Difficulty: Medium
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int[][] spiralMatrix(int m, int n, ListNode head) {
+
+        int[][] result = new int[m][n];
+
+        for (int [] row: result) {
+            Arrays.fill(row, -1);
+        }
+
+        int left=0,right=n-1,top=0, bottom=m-1;
+
+        while (head != null) {
+
+            for (int k=left; k<= right && head != null;k++) {
+                result[top][k] = head.val;
+                head = head.next;
+            }
+            top++;
+
+            for (int k=top; k<=bottom && head != null; k++) {
+                result[k][right] = head.val;
+                head = head.next;
+            }
+            right--;
+            
+            for (int k=right; k>=left && head != null; k--) {
+                result[bottom][k] = head.val;
+                head = head.next;
+            }
+            bottom--;
+            
+            for (int k=bottom; k>=top && head != null;k--) {
+                result[k][left] = head.val;
+                head = head.next;
+            }
+            left++;
+
+        
+        }
+
+        return result;
+    }
+}
