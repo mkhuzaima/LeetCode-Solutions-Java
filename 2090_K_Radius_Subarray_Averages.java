764f560d27198566058b845335443ff2340af0c7
Muhammad Khuzaima Umair <mkhuzaimaumair@gmail.com>
2023-06-20 14:09:07 +0500

    Create 2090_K_Radius_Subarray_Averages.java

---
+++ 2090_K_Radius_Subarray_Averages.java
@@ -0,0 +1,50 @@
+// id: 2090
+// Name: K Radius Subarray Averages
+// link: https://leetcode.com/problems/k-radius-subarray-averages/
+// Difficulty: Medium
+
+class Solution {
+    public int[] getAverages(int[] nums, int k) {
+
+        int subArraySize = 2 * k + 1;
+
+        int n = nums.length;
+
+        // create a result array
+        int [] result = new int[n];
+        
+        int i = 0;
+
+        long sum = 0;
+
+        while ( i < k && i < n) {
+            result[i] = -1;
+            sum += nums[i];
+            i++;
+        }
+
+        // add k more elements to the sum
+        for (int j = i; j < k+i && j < n; j++) {
+            sum += nums[j];
+        }
+
+        while (i+k<n) {
+            // add the rightmost element 
+            sum += nums[i+k];
+
+            result[i] = (int)(sum/subArraySize);
+
+            // remove element from left
+            sum -= nums[i-k];
+
+            i++;
+        }
+
+        while ( i < n) {
+            result [i] = -1;
+            i++;
+        }
+
+        return result;
+    }
+}
