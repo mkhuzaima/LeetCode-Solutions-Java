97c59c385b2ebe2a2ef3c5bc8e9428e3ac6588aa
Muhammad Khuzaima Umair <mkhuzaimaumair@gmail.com>
2023-10-02 15:39:45 +0500

    Create Problem 2038

---
+++ 2038_Remove_Colored_Pieces_if_Both_Neighbors_are_the_Same_Color.java
@@ -0,0 +1,23 @@
+// id: 2038
+// Name: Remove Colored Pieces if Both Neighbors are the Same Color
+// link: https://leetcode.com/problems/remove-colored-pieces-if-both-neighbors-are-the-same-color/description/
+// Difficulty: Medium
+
+class Solution {
+    public boolean winnerOfGame(String colors) {
+        int aliceMoves = 0;
+        int bobMoves = 0;
+        
+        for (int i = 1; i < colors.length() - 1; i++) {
+            if (colors.charAt(i-1) == colors.charAt(i) && colors.charAt(i) == colors.charAt(i+1)) {
+                if (colors.charAt(i) == 'A') {
+                    aliceMoves++;
+                } else {
+                    bobMoves++;
+                }
+            }
+        }
+
+        return aliceMoves > bobMoves;
+    }
+}
