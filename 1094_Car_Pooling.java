8b78a2701d5225ab0e6b0691fa940207af569841
Muhammad Khuzaima Umair <mkhuzaimaumair@gmail.com>
2023-10-12 20:08:08 +0500

    Create 1094_Car_Pooling.java

---
+++ 1094_Car_Pooling.java
@@ -0,0 +1,43 @@
+// id: 1094
+// Name: Car Pooling
+// link: https://leetcode.com/problems/car-pooling/
+// Difficulty: Medium
+
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        Arrays.sort(trips, ( a, b) -> a[1] - b[1]);
+
+        int currentPassengerCount = 0;
+
+        int distance = 0;
+
+        // min heap (pair = to, numPassengers)
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[2] - b[2]);
+        
+        // add capacity passengers. 
+        for (int[] trip: trips) {
+            int numPassengers = trip[0];
+
+            // System.out.println("disctance is " + distance);
+
+            // we have travelled till start distance;
+            distance = trip[1];
+
+            while (!pq.isEmpty() && pq.peek()[2] <= distance){
+                // System.out.println("peek is " + Arrays.toString(pq.peek()));
+                currentPassengerCount -= pq.peek()[0];
+                pq.remove();
+            }
+
+
+            if ((numPassengers + currentPassengerCount) > capacity) {
+                return false;
+            }
+
+            pq.add( trip );
+            currentPassengerCount += numPassengers;
+        }
+
+        return true;
+    }
+}
