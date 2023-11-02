dcc12ed9e773986bf54028807ea5287024e44e53
Muhammad Khuzaima Umair <mkhuzaimaumair@gmail.com>
2023-11-02 22:47:07 +0500

    Create 2265_Count_Nodes_Equal_to_Average_of_Subtree.java

---
+++ 2265_Count_Nodes_Equal_to_Average_of_Subtree.java
@@ -0,0 +1,47 @@
+// id: 2265
+// Name: Count Nodes Equal to Average of Subtree
+// link: https://leetcode.com/problems/count-nodes-equal-to-average-of-subtree/
+// Difficulty: Medium
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int count = 0;
+
+    int [] tuple = new int[3]; // match in tree, count in tree, sum of node values
+
+    public int averageOfSubtree(TreeNode root) {
+        // count nodes having value = average;
+        return countNodes(root)[0];
+    }
+
+    private int[] countNodes(TreeNode root) {
+        if (root == null) return new int[]{0, 0, 0};
+
+        int[] left = countNodes(root.left);
+        int[] right = countNodes(root.right);
+
+        int [] result = new int[]{left[0]+right[0], left[1]+right[1]+1, left[2]+right[2]+root.val};
+
+        int average = result[2]/result[1];
+
+        if (root.val == average) {
+            result[0]++;
+        }
+        return result;
+    }
+
+}
