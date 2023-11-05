9a69be2b7d9df6e293b8145ffced5a0048e3021b
Muhammad Khuzaima Umair <mkhuzaima@users.noreply.github.com>
2023-11-05 07:17:38 +0000

    Formatted 1535_Find_the_Winner_of_an_Array_Game.java

--- 1535_Find_the_Winner_of_an_Array_Game.java
+++ 1535_Find_the_Winner_of_an_Array_Game.java
@@ -1,7 +1,13 @@
-// id: 1535
-// Name: Find the Winner of an Array Game
-// link: https://leetcode.com/problems/find-the-winner-of-an-array-game/
-// Difficulty: Medium
+/*
+ * 1535. Find the Winner of an Array Game
+ * Problem Link: https://leetcode.com/problems/find-the-winner-of-an-array-game/
+ * Difficulty:   Medium
+ * 
+ * Solution Created by: Muhammad Khuzaima Umair
+ * LeetCode : https://leetcode.com/mkhuzaima/
+ * Github   : https://github.com/mkhuzaima
+ * LinkedIn : https://www.linkedin.com/in/mkhuzaima/
+ */
 
 class Solution {
     public int getWinner(int[] arr, int k) {
