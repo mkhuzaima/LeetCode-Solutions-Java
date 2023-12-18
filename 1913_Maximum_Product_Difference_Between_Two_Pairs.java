05a85f6d2d658de0f25ead2855c562bd40133d23
Muhammad Khuzaima Umair <mkhuzaima@users.noreply.github.com>
2023-12-18 16:16:55 +0000

    Formatted 1913_Maximum_Product_Difference_Between_Two_Pairs.java

--- 1913_Maximum_Product_Difference_Between_Two_Pairs.java
+++ 1913_Maximum_Product_Difference_Between_Two_Pairs.java
@@ -1,7 +1,13 @@
-// id: 1913
-// Name: Maximum Product Difference Between Two Pairs
-// link: https://leetcode.com/problems/maximum-product-difference-between-two-pairs/
-// Difficulty: Easy
+/*
+ * 1913. Maximum Product Difference Between Two Pairs
+ * Problem Link: https://leetcode.com/problems/maximum-product-difference-between-two-pairs/
+ * Difficulty:   Easy
+ * 
+ * Solution Created by: Muhammad Khuzaima Umair
+ * LeetCode : https://leetcode.com/mkhuzaima/
+ * Github   : https://github.com/mkhuzaima
+ * LinkedIn : https://www.linkedin.com/in/mkhuzaima/
+ */
 
 class Solution {
     public int maxProductDifference(int[] nums) {
